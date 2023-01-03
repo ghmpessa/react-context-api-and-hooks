bf2834a743fae464e6bf0681e5445d6d072d29b2
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-03 19:48:41 -0300

    create CounterContext, useCounterContext and Error throwing in case of not using context hook inside the context

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { CounterContextProvider } from './contexts/CounterContext'
 import './index.css'
-import App from './templates/App'
+import Home from './templates/Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <CounterContextProvider>
+      <Home />
+    </CounterContextProvider>
   </React.StrictMode>
 )
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,6 +1,8 @@
+import { useCounterContext } from '../../contexts/CounterContext'
 import './styles.css'
 
 export const Home = () => {
+  const [state, dispatch] = useCounterContext()
   return (
     <div>
       <h1>oi</h1>
