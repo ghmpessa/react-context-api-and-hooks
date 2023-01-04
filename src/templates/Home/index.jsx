6af3c8f99653880b93757605216d8514e40f08b6
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-03 21:06:42 -0300

    create Heading and Button components to apply context hook

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,11 +1,17 @@
 import { useCounterContext } from '../../contexts/CounterContext'
+import { Button } from '../../components/Button'
+import { Heading } from '../../components/Heading'
 import './styles.css'
 
 export const Home = () => {
   const [state, actions] = useCounterContext()
   return (
     <div>
-      <h1 onClick={() => actions.increase()}>oi</h1>
+      <Heading />
+      <div>
+        <Button onButtonClick={() => actions.increase()}>Increase</Button>
+        <Button onButtonClick={() => actions.increase()}>Decrease</Button>
+      </div>
     </div>
   )
 }
